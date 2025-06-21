3afd2e1e052acb254b01010e3c44062a02cf970b
Arthur Buitrago <arthurabf72@icloud.com>
2025-06-21 02:44:54 -0300

    Configuração Render 2

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-// server.js
 const jsonServer = require('json-server')
+const path = require('path')
 const server = jsonServer.create()
-const router = jsonServer.router('db/db.json')
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router(path.join(__dirname, 'db', 'db.json'))
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'public') // <-- isso serve seus HTMLs
+})
 
 server.use(middlewares)
+
+// Resposta customizada para a raiz /
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'))
+})
+
 server.use(router)
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
